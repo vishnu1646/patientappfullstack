4e661415dc7b5eece2fe021fef09a1fd1173f783
vishnu1646 <vishnulance98@gmail.com>
2022-12-07 14:51:45 +0530

    add

---
+++ src/app/addpatient/addpatient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addpatient',
+  templateUrl: './addpatient.component.html',
+  styleUrls: ['./addpatient.component.css']
+})
+export class AddpatientComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddpatientComponent } from './addpatient/addpatient.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddpatientComponent
+}]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddpatientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
